6d31f013153d7de5eb68179c2251ccbae0551c56
phaedrusflow <map@qompass.ai>
2025-05-30 21:39:18 -0700

    update

--- flake.nix
+++ flake.nix
@@ -3,124 +3,167 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
 
-      allSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit system;
-      });
+    allSystems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
+        });
 
-      installer_minimal_config = {
-        imports = [
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          self.nixosModules.default
-        ];
-        # Avoids a bunch of extra modules we don't have in the tegra_defconfig, like "ata_piix",
-        disabledModules = [ "profiles/all-hardware.nix" ];
+    installer_minimal_config = {
+      imports = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        self.nixosModules.default
+      ];
+      # Avoids a bunch of extra modules we don't have in the tegra_defconfig, like "ata_piix",
+      disabledModules = ["profiles/all-hardware.nix"];
 
-        hardware.nvidia-jetpack.enable = true;
+      hardware.nvidia-jetpack.enable = true;
+    };
+    aarch64_config = {
+      nixpkgs = {
+        buildPlatform = "aarch64-linux";
+        hostPlatform = "aarch64-linux";
       };
-      aarch64_config = {
-        nixpkgs = {
-          buildPlatform = "aarch64-linux";
-          hostPlatform = "aarch64-linux";
-        };
+    };
+    aarch64_cross_config = {
+      nixpkgs = {
+        buildPlatform = "x86_64-linux";
+        hostPlatform = "aarch64-linux";
       };
-      aarch64_cross_config = {
-        nixpkgs = {
-          buildPlatform = "x86_64-linux";
-          hostPlatform = "aarch64-linux";
-        };
-
+    };
+  in {
+    nixosConfigurations = {
+      installer_minimal = nixpkgs.lib.nixosSystem {
+        modules = [aarch64_config installer_minimal_config];
       };
-    in
-    {
-      nixosConfigurations = {
-        installer_minimal = nixpkgs.lib.nixosSystem {
-          modules = [ aarch64_config installer_minimal_config ];
-        };
-        installer_minimal_cross = nixpkgs.lib.nixosSystem {
-          modules = [ aarch64_cross_config installer_minimal_config ];
-        };
+      installer_minimal_cross = nixpkgs.lib.nixosSystem {
+        modules = [aarch64_cross_config installer_minimal_config];
       };
+    };
 
-      nixosModules.default = import ./modules/default.nix;
+    nixosModules.default = import ./modules/default.nix;
 
-      overlays.default = import ./overlay.nix;
+    overlays.default = import ./overlay.nix;
 
-      packages = {
-        x86_64-linux =
-          let
-            supportedConfigurations = lib.listToAttrs (map
-              (c: {
-                name = "${c.som}" + (lib.optionalString (c.super or false) "-super") + "-${c.carrierBoard}";
-                value = c;
-              }) [
-              { som = "orin-agx"; carrierBoard = "devkit"; }
-              { som = "orin-agx-industrial"; carrierBoard = "devkit"; }
-              { som = "orin-nx"; carrierBoard = "devkit"; }
-              { som = "orin-nano"; carrierBoard = "devkit"; }
-              { som = "orin-nx"; carrierBoard = "devkit"; super = true; }
-              { som = "orin-nano"; carrierBoard = "devkit"; super = true; }
-              { som = "xavier-agx"; carrierBoard = "devkit"; }
-              { som = "xavier-agx-industrial"; carrierBoard = "devkit"; } # TODO: Entirely untested
-              { som = "xavier-nx"; carrierBoard = "devkit"; }
-              { som = "xavier-nx-emmc"; carrierBoard = "devkit"; }
-            ]);
+    packages = {
+      x86_64-linux = let
+        supportedConfigurations = lib.listToAttrs (map
+          (c: {
+            name = "${c.som}" + (lib.optionalString (c.super or false) "-super") + "-${c.carrierBoard}";
+            value = c;
+          }) [
+            {
+              som = "orin-agx";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "orin-agx-industrial";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "orin-nx";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "orin-nano";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "orin-nx";
+              carrierBoard = "devkit";
+              super = true;
+            }
+            {
+              som = "orin-nano";
+              carrierBoard = "devkit";
+              super = true;
+            }
+            {
+              som = "xavier-agx";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "xavier-agx-industrial";
+              carrierBoard = "devkit";
+            } # TODO: Entirely untested
+            {
+              som = "xavier-nx";
+              carrierBoard = "devkit";
+            }
+            {
+              som = "xavier-nx-emmc";
+              carrierBoard = "devkit";
+            }
+          ]);
 
-            supportedNixOSConfigurations = lib.mapAttrs
-              (n: c: (nixpkgs.lib.nixosSystem {
-                modules = [
-                  aarch64_cross_config
-                  self.nixosModules.default
-                  {
-                    hardware.nvidia-jetpack = { enable = true; } // c;
-                    networking.hostName = "${c.som}-${c.carrierBoard}"; # Just so it sets the flash binary name.
-                  }
-                ];
-              }).config)
-              supportedConfigurations;
+        supportedNixOSConfigurations =
+          lib.mapAttrs
+          (n: c:
+            (nixpkgs.lib.nixosSystem {
+              modules = [
+                aarch64_cross_config
+                self.nixosModules.default
+                {
+                  hardware.nvidia-jetpack = {enable = true;} // c;
+                  networking.hostName = "${c.som}-${c.carrierBoard}"; # Just so it sets the flash binary name.
+                }
+              ];
+            }).config)
+          supportedConfigurations;
 
-            flashScripts = lib.mapAttrs' (n: c: lib.nameValuePair "flash-${n}" c.system.build.flashScript) supportedNixOSConfigurations;
-            initrdFlashScripts = lib.mapAttrs' (n: c: lib.nameValuePair "initrd-flash-${n}" c.system.build.initrdFlashScript) supportedNixOSConfigurations;
-            uefiCapsuleUpdates = lib.mapAttrs' (n: c: lib.nameValuePair "uefi-capsule-update-${n}" c.system.build.uefiCapsuleUpdate) supportedNixOSConfigurations;
-          in
-          {
-            # TODO: Untested
-            iso_minimal = self.nixosConfigurations.installer_minimal_cross.config.system.build.isoImage;
+        flashScripts = lib.mapAttrs' (n: c: lib.nameValuePair "flash-${n}" c.system.build.flashScript) supportedNixOSConfigurations;
+        initrdFlashScripts = lib.mapAttrs' (n: c: lib.nameValuePair "initrd-flash-${n}" c.system.build.initrdFlashScript) supportedNixOSConfigurations;
+        uefiCapsuleUpdates = lib.mapAttrs' (n: c: lib.nameValuePair "uefi-capsule-update-${n}" c.system.build.uefiCapsuleUpdate) supportedNixOSConfigurations;
+      in
+        {
+          # TODO: Untested
+          iso_minimal = self.nixosConfigurations.installer_minimal_cross.config.system.build.isoImage;
 
-            inherit (self.legacyPackages.x86_64-linux)
-              board-automation python-jetson;
-            inherit (self.legacyPackages.x86_64-linux.cudaPackages)
-              nsight_systems_host nsight_compute_host;
-          }
-          # Flashing and board automation scripts _only_ work on x86_64-linux
-          // flashScripts
-          // initrdFlashScripts
-          // uefiCapsuleUpdates;
+          inherit
+            (self.legacyPackages.x86_64-linux)
+            board-automation
+            python-jetson
+            ;
+          inherit
+            (self.legacyPackages.x86_64-linux.cudaPackages)
+            nsight_systems_host
+            nsight_compute_host
+            ;
+        }
+        # Flashing and board automation scripts _only_ work on x86_64-linux
+        // flashScripts
+        // initrdFlashScripts
+        // uefiCapsuleUpdates;
 
-        aarch64-linux = {
-          iso_minimal = self.nixosConfigurations.installer_minimal.config.system.build.isoImage;
-        };
+      aarch64-linux = {
+        iso_minimal = self.nixosConfigurations.installer_minimal.config.system.build.isoImage;
       };
+    };
 
-      checks = forAllSystems ({ pkgs, ... }: {
-        formatting = pkgs.runCommand "repo-formatting" { nativeBuildInputs = with pkgs; [ nixpkgs-fmt ]; } ''
-          nixpkgs-fmt --check ${self} && touch $out
-        '';
-      });
+    checks = forAllSystems ({pkgs, ...}: {
+      formatting = pkgs.runCommand "repo-formatting" {nativeBuildInputs = with pkgs; [nixpkgs-fmt];} ''
+        nixpkgs-fmt --check ${self} && touch $out
+      '';
+    });
 
-      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+    formatter = forAllSystems ({pkgs, ...}: pkgs.nixpkgs-fmt);
 
-      legacyPackages = forAllSystems ({ system, ... }:
+    legacyPackages = forAllSystems (
+      {system, ...}:
         (import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true;
-            cudaCapabilities = [ "7.2" "8.7" ];
+            cudaCapabilities = ["7.2" "8.7"];
             cudaSupport = true;
           };
           overlays = [
@@ -133,10 +176,10 @@
               inherit (final.nvidia-jetpack) cudaPackages;
               # TODO: Remove after bumping past 24.11: reset OpenCV's override on cudaPackages.
               # https://github.com/NixOS/nixpkgs/blob/7ffe0edc685f14b8c635e3d6591b0bbb97365e6c/pkgs/top-level/all-packages.nix#L10540-L10541
-              opencv4 = prev.opencv4.override { inherit (final) cudaPackages; };
+              opencv4 = prev.opencv4.override {inherit (final) cudaPackages;};
             })
           ];
         }).nvidia-jetpack
-      );
-    };
+    );
+  };
 }
